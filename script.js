05cfdb854932e1ffb39240da3d22db589dda99aa
JoaoVitor-Bispo <joaovitor.bispo080@gmail.com>
2022-10-24 22:14:27 -0300

    ATT

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrir_menu() {
+    var conteudo = window.document.getElementById('allpergs')
+    if (conteudo.style.display == 'none') {
+        conteudo.style.display = 'block'
+    }
+    else {
+        conteudo.style.display = 'none'
+    }
+}
+
