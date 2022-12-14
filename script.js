cc7d8c321a0f24bde7b19f8803913347b4a6390d
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2022-12-14 18:42:53 -0300

    map

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function abrir_menu() {
-    var conteudo = window.document.getElementById('allpergs')
-    if (conteudo.style.display == 'none') {
-        conteudo.style.display = 'block'
-    }
-    else {
-        conteudo.style.display = 'none'
-    }
-}
+const div = [...document.querySelectorAll('.perguntas')]
+const p = [...document.querySelectorAll('.escondido')]
+
+div.map((el) => {
+    el.addEventListener('click', (x) =>  {
+        let el2 = x.target
+        el2.style.color = 'red'
+    })
+})
 
