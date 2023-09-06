677e63145322bf60c6c40e6bf959662f8a07ab89
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-09-06 14:55:43 -0300

    .

--- script.js
+++ script.js
@@ -3,17 +3,8 @@ const p = document.querySelectorAll('.escondido')
 
 div.map((el) => {
     el.addEventListener('click', (x) =>  {
-        const child_element = el.children.item(1);
-        const close = el.children.item(0)
-        console.log(child_element)
-        if (child_element.style.display == 'block') {
-            close.style.display = 'block'
-            child_element.style.display = 'none'
-        }
-        else {
-            close.style.display = 'none'
-            child_element.style.display = 'block'
-        }
+        const childElement = x.target.children[1]
+        const iconElement = x.target.children[0]
     })
 })
 
